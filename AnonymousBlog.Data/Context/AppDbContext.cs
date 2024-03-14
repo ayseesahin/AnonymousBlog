f84e38914e134fc7c5f7978eca77822144939e54
aysenursahin <ayse214350@ogr.duzce.edu.tr>
2024-03-15 02:56:02 +0300

    I got ConnectionString from appsetting

---
+++ AnonymousBlog.Data/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using AnonymousBlog.Entity.Entities;
+
+
+namespace AnonymousBlog.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+
+
+    }
+}
+
