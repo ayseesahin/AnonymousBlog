c9a1402717818216c8bd8e4edcbf587de764b0f0
aysenursahin <ayse214350@ogr.duzce.edu.tr>
2024-03-15 18:02:17 +0300

    Generic Repository Pattern and Dependency Injection

---
+++ AnonymousBlog.Data/Extensions/DataLayerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AnonymousBlog.Data.Repositories.Abstractions;
+using AnonymousBlog.Data.Repositories.Concretes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AnonymousBlog.Data.Extensions
+{
+	public static class DataLayerExtensions
+	{
+		public static IServiceCollection LoadDataLayerExtension(this IServiceCollection services, IConfiguration config)
+		{
+			services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+			return services;
+		}
+	}
+}
+
